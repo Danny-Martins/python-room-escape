8d2ea791814a6ead01ddce9b5a29706b1aa9ae90
Danny-Martins <Danny_Martins50@hotmail.com>
2015-11-30 15:29:15 -0500

    rooms completed
    added lockable rooms
    all rooms are now random minus rooms c,f, final

--- main.py
+++ main.py
@@ -6,37 +6,13 @@ import time
 import os
 
 class makeRoom(object):
-	def __init__(self, name, items=None):
+	def __init__(self, name, items=None, locked=None):
 		self.items = items or []
 		self.name = name
-		self.exit_north = None
-		self.exit_east = None
-		self.exit_south = None
-		self.exit_west = None
+		self.locked = locked
 
 	def __repr__(self):
 		return self.name
-
-	# legacey code , more for testing at this point
-	def attach_next_to(self, parent_room):
-		random_temp = random.randint(1,4)
-		if random_temp == 1:
-			self.exit_north = parent_room
-			parent_room.exit_south = self
-		elif random_temp == 2:
-			self.exit_east = parent_room
-			parent_room.exit_west = self
-		elif random_temp == 3:
-			self.exit_south = parent_room
-			parent_room.exit_north = self
-		else:
-			self.exit_west = parent_room
-			parent_room.exit_east = self
-
-	# ignore this , used to test certain rooms without going thru game etc
-	def attach_next_to_debug(self, parent_room):
-		self.exit_south = parent_room
-		parent_room.exit_north = self
 
 class player(object):
 	def __init__(self, position):
@@ -51,9 +27,7 @@ class player(object):
 			print(item)
 
 	def print_options(self):
-		print('You are in %s. Here are your options for exits:' % self.position.name)
-		print(' \nnorth: %s, east: %s, south: %s, west: %s ,type take for pickup items' 
-			% (self.position.exit_north, self.position.exit_east, self.position.exit_south ,self.position.exit_west))
+		print('You are in %s.' % self.position.name)
 
 	def print_room_items(self):
 		print('these are the items in the room:')
@@ -86,60 +60,56 @@ class player(object):
 				print("not item in your backpack")
 				time.sleep(2.5)
 
-	def check_input(self, choice):
-		if choice == "north" and self.position.exit_north:
-			self.position = self.position.exit_north
-		elif choice == "east" and self.position.exit_east:
-			self.position = self.position.exit_east
-		elif choice == "south" and self.position.exit_south:
-			self.position = self.position.exit_south
-		elif choice == "west" and self.position.exit_west:
-			self.position = self.position.exit_west
-		else:
-			print("you typed something wrong")
-
 	def grid_move(self, choice):
 		self.move_items(choice)
-		if choice == "north" and self.x !=0:
+		if choice == "n" and self.x !=0 and all_rooms[self.x - 1][self.y].locked != True:
 			self.x -= 1
 			self.position = all_rooms[self.x][self.y]
-		elif choice == "east" and self.y !=2:
+		elif choice == "e" and self.y !=2 and all_rooms[self.x][self.y + 1].locked != True:
 			self.y += 1
 			self.position = all_rooms[self.x][self.y]
-		elif choice == "south" and self.x !=1:
+		elif choice == "s" and self.x !=3 and all_rooms[self.x + 1][self.y].locked != True:
 			self.x += 1
 			self.position = all_rooms[self.x][self.y]
-		elif choice == "west" and self.y !=0:
+		elif choice == "w" and self.y !=0 and all_rooms[self.x][self.y - 1].locked != True:
 			self.y -= 1
 			self.position = all_rooms[self.x][self.y]
 		else:
 			print("locked or you typed something wrong")
 
 # how the game must be layed out,  
-# item 1 in room a , item 1 used in room b which unlocks c 
+# item 1 in room a , item 1 used in room B which unlocks C 
 # item 2 in room c , item 2 used in room D which drops item 3
 # item 4 in room E, unlocks F , room F leads to final room
 # item 3 used in final room
 
-room_a = makeRoom("Room A", ["Rope"])
+room_a = makeRoom("Room A", ["rope"])
 room_b = makeRoom("Room B")
-room_c = makeRoom("Room C", ["Knife"])
+room_c = makeRoom("Room C", ["knife"], True)
 room_d = makeRoom("Room D")
-room_e = makeRoom("Room E", ["Crowbar"])
-room_f = makeRoom("Room F")
+room_e = makeRoom("Room E", ["crowbar"])
+room_f = makeRoom("Room F", ["test"], True)
+room_g = makeRoom("Room G")
+room_h = makeRoom("Room H")
+room_i = makeRoom("Room I")
+room_j = makeRoom("Room J")
+room_k = makeRoom("Room K")
+room_final = makeRoom("Room final", ["test"], True)
 
-room_test1 = makeRoom("test1")
-room_test2 = makeRoom("test2")
-
-random_rooms = [room_a, room_b, room_c, room_d, room_e, room_f]
+random_rooms = [room_a, room_b, room_g, room_d, room_e, room_h, room_i, 
+				room_j, room_k]
 random.shuffle(random_rooms)
 
 first_room_row = [random_rooms[0],random_rooms[1],random_rooms[2]]
 second_room_row = [random_rooms[3],random_rooms[4],random_rooms[5]]
+third_room_row = [random_rooms[6],random_rooms[7],random_rooms[8]]
+final_room_row = [room_c, room_f, room_final]
 
 all_rooms = []
 all_rooms.append(first_room_row)
 all_rooms.append(second_room_row)
+all_rooms.append(third_room_row)
+all_rooms.append(final_room_row)
 
 flamie = player(all_rooms[0][1])
 
